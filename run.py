da73f4bd5e180a2ed774184bad77132e842612c3
thibalbo <thiago.dbalbo@gmail.com>
2017-03-10 15:02:51 -0300

    [skip ci] later handler import to avoid None value environment variables

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import logging
 import sys
 import os
 import fire
-from pyskel import handle
 
 logger = logging.getLogger(__name__)
 ENVIRONMENTS = ['local', 'qa', 'prod']
@@ -16,19 +15,20 @@ def main(handler, env):
     :param handler: handler to be executed
     :param env: environment (one of: local | qa | prod)
     """
+    if env in ENVIRONMENTS:
+        os.environ['ENVIRONMENT'] = env
+    else:
+        logger.error('Wrong environment choice=%s.', env)
+        sys.exit(1)
+
     try:
+        # later import to void loading all modules before adding environment variable to environment
+        # to avoid any global variable loading None or from config file
+        from pyskel import handle
         _handler = getattr(handle, handler)
     except AttributeError:
         logger.error('Handler not found.', exc_info=True)
         sys.exit(1)
-
-    if env in ENVIRONMENTS:
-        _env = env
-    else:
-        logger.warning('Wrong environment choice=%s.', env)
-        sys.exit(1)
-
-    os.environ['ENVIRONMENT'] = _env
 
     event, context = None, None
     return _handler(event, context)
