2e286b15770afbcc852c235c1b907c4b3fb7cff9
thibalbo <thiago.dbalbo@gmail.com>
2017-06-12 15:47:05 -0300

    load logger

--- pyskel/__init__.py
+++ pyskel/__init__.py
@@ -2,11 +2,19 @@ __author__ = 'Thiago Balbo'
 __email__ = 'thiago.dbalbo@gmail.com'
 __version__ = '0.1.0'
 
+import logging.config
+import os
+from pyskel import config
+import yaml
 import dotenv
-from log import log
 
 # load environment variables
 dotenv.load_dotenv('.env')
 
-# config file load must come before getting logger
-log.setup_logging(default_path='log/config.yml')
+# load logger
+log_directory = config.get_property('LOGDIR')
+log_filename = config.get_property('LOGFILENAME')
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+logging_cfg = yaml.safe_load(open(log_filename, 'rt'))
+logging.config.dictConfig(logging_cfg)
