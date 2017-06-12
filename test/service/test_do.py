cd495bcecf42d7afdf753b6ebad2539dee8c712b
thibalbo <thiago.dbalbo@gmail.com>
2017-06-12 16:46:58 -0300

    clean tests

--- test/service/test_do.py
+++ test/service/test_do.py
@@ -1,6 +1,1 @@
 """test.service.test_do"""
-from pyskel.service import do
-
-
-def test_run():
-    assert do.run() is None
--- test/test_handle.py
+++ test/test_handle.py
@@ -1,8 +1,1 @@
 """test.test_handle"""
-from pyskel import handle
-
-
-def test_do_handle():
-    event, context = None, None
-    resp = handle.do_handle(event, context)
-    assert resp is None
